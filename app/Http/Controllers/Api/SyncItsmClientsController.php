7ceb834cd066118b22cc3c2258d93f96401ab0bd
Francis Cisneros <68484924+Francis-Cisneros@users.noreply.github.com>
2026-07-24 15:00:57 -0500

    fix

--- app/Http/Controllers/Api/SyncItsmClientsController.php
+++ app/Http/Controllers/Api/SyncItsmClientsController.php
@@ -227,7 +227,7 @@ class SyncItsmClientsController extends Controller
                 | Enviar atenciones Cybernet como followups a Fortuna
                 |--------------------------------------------------------------------------
                 */
-                $atenciones = CybAtencion::where('idTicket', $ticket->idTicket)
+                $atenciones = cybAtencion::where('idTicket', $ticket->idTicket)
                     ->where('flgNoenviar', '0')
                     ->get();
 
